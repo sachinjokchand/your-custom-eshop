6699c7539431d045e55616ae5242d0b14224e435
unknown <sachincodingkart@gmail.com>
2022-02-18 14:22:39 +0530

    deploy

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,12 +3,35 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
   return (
-    <footer>
-      <Container>
-        <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
-        </Row>
-      </Container>
+    // <footer>
+    //   <Container>
+    //     <Row>
+    //       <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+    //     </Row>
+    //   </Container>
+    // </footer>
+    <footer style='background-color: rgb(59, 64, 68);'>
+      <div class='container'>
+        <div class='row'>
+          <div class='text-center p-3 col-sm-12' style='color: white;'>
+            Email - sachinjokchand@gmail.com
+          </div>
+          <div class='text-center p-3 col-sm-12' style='color: white;'>
+            LinkedIn -{' '}
+            <a
+              href='https://www.linkedin.com/in/sachin-jogchand-b4505a17a/'
+              style='color: white; text-decoration: none;'
+            >
+              https://www.linkedin.com/in/sachin-jogchand-b4505a17a/
+            </a>{' '}
+          </div>
+        </div>
+        <div class='row'>
+          <div class='text-center p-3 col' style='color: white;'>
+            Copyright © YourCustomeEshop
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
